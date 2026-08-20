cb2773ce3cde263cf94488885e2573192d62bf82
Archana-M-11 <archanamnair096@gmail.com>
2026-08-20 15:52:51 +0530

    Fix embedding model initialization

--- backend/app/services/retriever.py
+++ backend/app/services/retriever.py
@@ -1,8 +1,8 @@
-from app.vectorstore.embeddings import create_embeddings
+from app.vectorstore.embeddings import embeddings
 from app.vectorstore.chroma_store import load_vectore_store
 
 def retrieve_documents(query, k=4):
-    embeddings = create_embeddings()
+    # embeddings = create_embeddings()
 
     vector_store = load_vectore_store(embeddings)
 
